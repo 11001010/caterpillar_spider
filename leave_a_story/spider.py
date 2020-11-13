51f302f7721db632d0052d0eafc551d88e69506b
caterpillar-_-42 <gxt2048@163.com>
2020-11-13 16:54:09 +0800

    :bug: 增加水印

--- leave_a_story/spider.py
+++ leave_a_story/spider.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals, division
 import requests
 from bs4 import BeautifulSoup
 import urllib.request
 import re
+import os
+import sys
+import time
+os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
+from PIL import Image, ImageDraw, ImageFont
 
 pattern = re.compile(r'([^<>/\\\|:""\*\?]+)\.\w+$')
-
+#水印位置 需要减去四个像素
+test_marker_margin = 4
+textMark = "power @ leaveastory.com; spider@caterpillar"
+# 获取图片
 def getImg(site_url: str,flag):
     # 访问网站
     url = requests.get(site_url)
@@ -31,16 +40,40 @@ def parser_img_tags(imgTags,flag):
             imgs_set_str = imgTag.get("data-vpf-srcset")
             for imgs_url_and_size_str in imgs_set_str.split(","):
                 save_image(imgs_url_and_size_str)
-# 保存图片            
+ 
+def add_water_marker( imgPath):
+    image = Image.open(imgPath)
+    # 实例化图片对象
+    img = Image.open(imgPath)
+    
+    # 获取图片的宽、高,以便计算图片的相对位置
+    w, h = img.size  
+    
+    #查看图片高度
+    # print("图片高度：",h)
+    # print("图片宽度：",w)
+    # 设置字体、字体大小
+    font = ImageFont.truetype('C:/Users/Administrator/SourceHanSansCN-Regular.ttf',22)
+    draw = ImageDraw.Draw(img)
+    font_x,font_y =font.getsize(textMark)
+    #draw.text的四个参数设置:文字位置(横坐标，纵坐标)/内容/颜色/字体
+    draw.text((w - font_x - test_marker_margin,h - font_y - test_marker_margin), text=textMark, fill=(0, 0, 0,0), font=font)
+    img.save(imgPath)
+
+# 保存图片、加水印      
 def save_image(imgs_url_and_size_str):
     # https://leaveastory.com/wp-content/uploads/2018/03/359-500x500.png 500w
     img_url = imgs_url_and_size_str.split(" ")[0]
     img_name = pattern.findall(img_url)
     if(img_name):
-        urllib.request.urlretrieve(img_url,"./leave_a_story/image/%s.png" % img_name[0])
+        img_full_name = "./leave_a_story/image/%s.png" % img_name[0]
+        urllib.request.urlretrieve(img_url,img_full_name)
+        time.sleep(3)
+        add_water_marker(img_full_name)
+        time.sleep(3)
 if __name__ == "__main__":
     i = 0
     flag = 1
-    for i  in  range(1,2):
-
+    # 仅下载前9页
+    for i  in  range(1,10):
         getImg("https://leaveastory.com/?vp_page=%s" % str(i),flag)
