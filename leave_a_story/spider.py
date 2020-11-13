1c7d5a8af824ebd255c1906c9a39eb20e81f4b08
caterpillar-_-42 <gxt2048@163.com>
2020-11-13 10:44:06 +0800

    :bug: 调整代码

--- leave_a_story/spider.py
+++ leave_a_story/spider.py
@@ -6,7 +6,7 @@ import re
 
 pattern = re.compile(r'([^<>/\\\|:""\*\?]+)\.\w+$')
 
-def getImg(site_url: str):
+def getImg(site_url: str,flag):
     # 访问网站
     url = requests.get(site_url)
     # 获取网站数据
@@ -17,16 +17,30 @@ def getImg(site_url: str):
     # 获取所有img标签
     imgTags = soup.find_all("img")
     print(len(imgTags))
-    for imgTag in imgTags:
-        # 单张图片
-        # imgUrl = imgTag.get("data-vpf-src")
+    # 解析img标签 
+    parser_img_tags(imgTags,flag)
 
-        # 不同尺寸大小的图片
-        imgs_set_str = imgTag.get("data-vpf-srcset")
-        for imgs_url_and_size_str in imgs_set_str.split(","):
-            # https://leaveastory.com/wp-content/uploads/2018/03/359-500x500.png 500w
-            img_url = imgs_url_and_size_str.split(" ")[0]
-            img_name = pattern.findall(img_url)
-            if(img_name):
-                urllib.request.urlretrieve(img_url,"./leave_a_story/image/%s.png" % img_name[0])
-getImg("https://leaveastory.com/?vp_page=2")
+# 解析img标签，获取图片链接
+def parser_img_tags(imgTags,flag):
+    for imgTag in imgTags:
+        if flag:
+            # 单张图片
+            save_image(imgTag.get("data-vpf-src"))
+        else:
+            # 不同尺寸大小的图片
+            imgs_set_str = imgTag.get("data-vpf-srcset")
+            for imgs_url_and_size_str in imgs_set_str.split(","):
+                save_image(imgs_url_and_size_str)
+# 保存图片            
+def save_image(imgs_url_and_size_str):
+    # https://leaveastory.com/wp-content/uploads/2018/03/359-500x500.png 500w
+    img_url = imgs_url_and_size_str.split(" ")[0]
+    img_name = pattern.findall(img_url)
+    if(img_name):
+        urllib.request.urlretrieve(img_url,"./leave_a_story/image/%s.png" % img_name[0])
+if __name__ == "__main__":
+    i = 0
+    flag = 1
+    for i  in  range(1,2):
+
+        getImg("https://leaveastory.com/?vp_page=%s" % str(i),flag)
